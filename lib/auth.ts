00566dc2fe07a2293791f5d8eb8bb2ab77e35872
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-12-20 17:49:17 +0600

    Add PrismaAdapter and CredentialsProvider to authOptions

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,9 +1,50 @@
+import prisma from '../prisma';
 import NextAuth from 'next-auth';
 import { NextAuthOptions } from 'next-auth';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import GoogleProvider from 'next-auth/providers/google';
+import CredentialsProvider from 'next-auth/providers/credentials';
 
 export const authOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: {
+          label: 'Email',
+          type: 'email',
+          placeholder: 'Email',
+        },
+        password: {
+          label: 'Password',
+          type: 'password',
+          placeholder: 'Password',
+        },
+      },
+      async authorize(credentials, req) {
+        if (!credentials?.email || !credentials.password) return null;
+
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        });
+
+        if (!user) {
+          throw new Error('Invalid Credentials');
+        }
+
+        const passwordsMatch = credentials.password === user?.password;
+
+        if (passwordsMatch) {
+          return {
+            ...user,
+            id: user.id.toString(),
+          };
+        } else {
+          throw new Error('Invalid Credentials');
+        }
+      },
+    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
