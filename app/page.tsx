61dc72de5fc9ff99350bb66c87ba1e346f41e9b7
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-12-30 17:54:41 +0600

    Fix import order in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { useTheme } from 'next-themes';
 import Link from 'next/link';
+import { useTheme } from 'next-themes';
 
 export default function Home() {
   const { theme, setTheme } = useTheme();
