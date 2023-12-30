60212a6f19fcad2d9b8269b9cd3c914a9aaa52ff
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-12-30 17:53:55 +0600

    Add NextAuth.js for authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,22 @@
 'use client';
 
 import { useTheme } from 'next-themes';
+import Link from 'next/link';
 
 export default function Home() {
   const { theme, setTheme } = useTheme();
   return (
     <div className='grid h-screen place-items-center'>
-      <h1 className='text-5xl font-bold'>Next.js + Tailwind CSS</h1>
+      <h1 className='text-5xl font-bold'>
+        Next.js + Tailwind CSS + NextAuth.js
+      </h1>
       <h2 className='text-2xl font-bold'>
         With Dark Mode {theme === 'dark' ? '🌙' : '☀️'}
       </h2>
       <button onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
         Toggle Theme
       </button>
+      <Link href='/api/auth/signin'>Sign In</Link>
     </div>
   );
 }
